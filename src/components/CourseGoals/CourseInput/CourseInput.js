b2d4d1f2021f8cad6e23b12a8b43cb4efe18a821
Victor Pinedo <victorpinedomartinez@gmail.com>
2023-07-15 21:22:45 -0500

    98. Setting CSS Classes Dynamically

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -11,6 +11,8 @@ const CourseInput = props => {
     setEnteredValue(event.target.value);
     if (event.target.value.trim().length > 0) {
       setIsValid(true);
+    } else {
+      setIsValid(false);
     }
   };
 
@@ -25,9 +27,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
-        <input style={{ borderColor: !isValid ? "red" : "black", backgroundColor: !isValid ? "salmon" : "transparent" }} type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${!isValid && 'invalid'}`}>
+         <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
